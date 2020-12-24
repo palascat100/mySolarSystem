6f6852f23bcc8722978cb88b6d7b0b0fcf6812ca
palascat100 <73373091+palascat100@users.noreply.github.com>
2020-12-23 23:27:15 -0500

    Update Calculations.java

--- mySolarSystem/src/mySolarSystem/Calculations.java
+++ mySolarSystem/src/mySolarSystem/Calculations.java
@@ -6,37 +6,6 @@ import java.util.ArrayList;
 public class Calculations 
 //class that does all calculations for orbiting objects
 {
-	double accel;
-	double tau=.01;
-	static ArrayList<Double> position= new ArrayList<Double>();
-	static ArrayList<Double> velocity= new ArrayList<Double>();
-	static ArrayList<Double> acceleration= new ArrayList<Double>();
-	public Calculations()
-	{
-		
-	}
-	private double CalcAccel(double mass, double radius)
-	{
-		//takes in mass with units 10^22 kg and km
-		accel=667.4*(mass)/(radius*radius*radius);
-		return accel;
-	}
-	
-	private void Verlet()
-	{
-		
-	}
-	
-}
-
-package mySolarSystem;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
-public class Calculations 
-//class that does all calculations for orbiting objects
-{
 	double tau=0.02;
 	static ArrayList<Double> position= new ArrayList<Double>();
 	static ArrayList<Double> currentpos= new ArrayList<Double>();
@@ -168,4 +137,3 @@ public class Calculations
 	}
 	
 }
-

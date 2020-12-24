998b79795db7e6b29c7b55f54c15805cd60e9044
palascat100 <73373091+palascat100@users.noreply.github.com>
2020-12-23 23:28:04 -0500

    Update SolarPanel.java

--- mySolarSystem/src/mySolarSystem/SolarPanel.java
+++ mySolarSystem/src/mySolarSystem/SolarPanel.java
@@ -3,6 +3,9 @@ package mySolarSystem;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.MouseInfo;
+import java.awt.PointerInfo;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -12,19 +15,26 @@ public class SolarPanel extends JPanel {
 	//initializes all necessary variables
 	Graphics2D g2d;
 	int determine;
-	int i=0;
+	int num=0;
 	int keeptrack=1;
 	int panelWidth, panelHeight;
 	JFrame frame;
+	Calculations calc;
+	int numBodies;
+	Main m;
+	static ArrayList<Double> position= new ArrayList<Double>();
+	
 	/**
 	 * Create the panel.
 	 */
-	public SolarPanel(JFrame frame1) 
+	public SolarPanel(Main main, JFrame frame1) 
 	{
 		//makes background blue
 		setBackground(new Color(0, 0, 128));
 		setForeground(new Color(0, 0, 128));
 		frame=frame1;
+		m=main;
+
 	}
 	public void paintComponent(Graphics g)
 	{
@@ -48,5 +58,67 @@ public class SolarPanel extends JPanel {
 		 }
 		 
 	}
+	
+	public void paintOrbits()
+	{
+		calc.calc();
+		position.addAll(calc.getPos());
+		numBodies=m.getNumBodies();
+		
+		Graphics g1=this.getGraphics();
+		if(determine==1)
+		{
+				
+				int n;
+				g2d=(Graphics2D) g1;
+				for(n=0;n<numBodies;n++)
+				{
+					getColor(n,g2d);
+					g2d.drawLine((int)Math.floor(position.get((num+n)*2))+400, (int)Math.floor(position.get((num+n)*2+1))+200, (int)Math.floor(position.get((num+n)*2+4))+400, (int)Math.floor(position.get((num+n)*2+5))+200);
+				}
+			
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+			num+=2;
+			m.checkNum();
+		}
+	}
+	
+	private void getColor(int i, Graphics2D g2d)
+	{
+		if(i==0)
+		{
+			g2d.setColor(Color.YELLOW);
+		}
+		if(i==1)
+		{
+			g2d.setColor(Color.MAGENTA);
+		}
+		if(i==2)
+		{
+			g2d.setColor(Color.CYAN);
+		}
+		if(i==3)
+		{
+			g2d.setColor(Color.GREEN);
+		}
+	}
+	
+	public void getNum(int num)
+	{
+		determine=num;
+	}
+	
+	public void getCalc(Calculations calculations)
+	{
+		calc=calculations;
+		position.addAll(calc.getInit());
+	}
+	
 
 }
